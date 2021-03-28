cc2748b3b99f522d952a08ff844bdfca856632eb
SriyaNelluri <60146450+SriyaNelluri@users.noreply.github.com>
2021-03-28 13:14:54 +0530

    Update 1a.cpp

--- week1/1a.cpp
+++ week1/1a.cpp
@@ -1,36 +1,80 @@
+// { Driver Code Starts
+//Initial template for C++
+
 #include <bits/stdc++.h>
 using namespace std;
 
-int printUnion(int arr1[], int arr2[], int m, int n)
-{
-	int i = 0, j = 0;
-	while (i < m && j < n) {
-		if (arr1[i] < arr2[j])
-			cout << arr1[i++] << " ";
+ // } Driver Code Ends
+//User function template in C++
 
-		else if (arr2[j] < arr1[i])
-			cout << arr2[j++] << " ";
+class Solution{
+    public:
+    //Function to return the count of number of elements in union of two arrays.
+    int doUnion(int a[], int n, int b[], int m)  {
+        //code here
+        int i=0;
+        int j=0;
+        int count=0;
+        //find maximum of two arrays and create size of max+1
+        int max=0;
+        for(i=0;i<n;i++){
+            if(max<a[i]){
+                max=a[i];
+            }
+        }    
+         for(j=0;i<m;j++){
+            if(max<b[j]){
+                max=b[j];
+            }
+         }    
+        int* res=(int*)malloc((max+1)*sizeof(int));
+        for(j=0;j<max+1;j++){
+            res[j]=0;
+        }
+        for(i=0;i<n;i++){
+            if(a[i]!=a[i+1]){
+                res[a[i]]+=1;
+            }
+        } 
+        for(i=0;i<m;i++){
+            if(res[b[i]]==0){
+                res[b[i]]+=1;
+            }
+        } 
+        for(i=0;i<max+1;i++){
+            if(res[i]!=0){
+                count++;
+            }
+        } 
+        
+        return count;
+   
+};
 
-		else {
-			cout << arr2[j++] << " ";
-			i++;
-		}
+// { Driver Code Starts.
+
+int main() {
+	
+	int t;
+	cin >> t;
+	
+	while(t--){
+	    
+	    int n, m;
+	    cin >> n >> m;
+	    int a[n], b[m];
+	   
+	    for(int i = 0;i<n;i++)
+	       cin >> a[i];
+	       
+	    for(int i = 0;i<m;i++)
+	       cin >> b[i];
+	    Solution ob;
+	    cout << ob.doUnion(a, n, b, m) << endl;
+	    
 	}
-
-	while (i < m)
-		cout << arr1[i++] << " ";
-
-	while (j < n)
-		cout << arr2[j++] << " ";
-}
-
-int main(){
-	int arr1[] = { 1, 2, 4, 5, 6 };
-	int arr2[] = { 2, 3, 5, 7 };
-	int m = sizeof(arr1) / sizeof(arr1[0]);
-	int n = sizeof(arr2) / sizeof(arr2[0]);
-	printUnion(arr1, arr2, m, n);
-
+	
 	return 0;
+}  // } Driver Code Endsreturn 0;
 }
 
