4fac0fb5fa0f1eee7ad74ce0b12eee64d5720a8f
SriyaNelluri <60146450+SriyaNelluri@users.noreply.github.com>
2021-03-28 13:45:56 +0530

    Update 11..cpp

--- week1/11..cpp
+++ week1/11..cpp
@@ -1,35 +1,47 @@
-#include <bits/stdc++.h>
-using namespace std;
-void reverse(int arr[], int start, int end)
-{
-	while (start < end)
-	{
-		int temp = arr[start];
-		arr[start] = arr[end];
-		arr[end] = temp;
-		start++;
-		end--;
-	}
-}	
-int main()
-{
-	int arr[] = {1, 2, 3, 4, 5, 6,7,8,9,10};
-	
-	int n = sizeof(arr) / sizeof(arr[0]);
-	for (int i = 0; i < n; i++){
-		cout << arr[i] << " ";
-	}
-	
-	reverse(arr, 0, n-1);
-	
-	cout << "after" << endl;
-	for (int i = 0; i < n; i++){
-		cout << arr[i] << " ";
-	}
-	
-	
+// { Driver Code Starts
+//Initial Template for C++
 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+string reverseWord(string str);
+
+
+int main() {
 	
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	string s;
+	cin >> s;
+	
+	cout << reverseWord(s) << endl;
+	}
 	return 0;
+	
+}
+
+// } Driver Code Ends
+
+
+//User function Template for C++
+
+string reverseWord(string str){
+    
+  //Your code here
+  int i=0;
+  int j=str.length()-1;
+  char temp;
+  while(i<j){
+        temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+        i++;
+        j--;
+  }
+  return str;
 }
 
