f075911e0ce45bfadce9ff5345480dee6fe41d37
SriyaNelluri <60146450+SriyaNelluri@users.noreply.github.com>
2021-03-28 13:21:40 +0530

    Update 2a.cpp

--- week1/2a.cpp
+++ week1/2a.cpp
@@ -1,43 +1,49 @@
-// C++ program to find the element
-// occurring odd number of times
+// { Driver Code Starts
+//Initial template for C++
+
 #include <bits/stdc++.h>
 using namespace std;
 
-// function to find the element
-// occurring odd number of times
-int getOddOccurrence(int arr[],int size)
-{
-	
-	// Defining HashMap in C++
-	unordered_map<int, int> hash;
+ // } Driver Code Ends
+//User function template for C++
 
-	// Putting all elements into the HashMap
-	for(int i = 0; i < size; i++)
-	{
-		hash[arr[i]]++;
-	}
-	// Iterate through HashMap to check an element
-	// occurring odd number of times and return it
-	for(auto i : hash)
-	{
-		if(i.second % 2 != 0)
-		{
-			return i.first;
-		}
-	}
-	return -1;
+class Solution{   
+public:
+    int getOddOccurrence(int arr[], int n) {
+        // code here
+        unordered_map <int,int> count;
+        int i=0;
+        for(i=0;i<n;i++){
+            count[arr[i]]++;
+        }
+        for(auto i : count){
+        if(i.second % 2 != 0)
+        {
+            return i.first;
+        }
+    }
+    return -1;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.getOddOccurrence(arr, n);
+        cout << ans << "\n";
+    }
+    return 0;
 }
-int main()
-{
-	int arr[] = { 2, 3, 5, 4, 5, 2, 4,
-					3, 5, 2, 4, 4, 2 };
-	int n = sizeof(arr) / sizeof(arr[0]);
-	
-	// Function calling
-	cout << getOddOccurrence(arr, n);
 
-	return 0;
-}
-
+  // } Driver Code Ends
 
 
