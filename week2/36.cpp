d10c30566cc9c56742245205ada76302ad02801c
SriyaNelluri <60146450+SriyaNelluri@users.noreply.github.com>
2021-04-10 19:45:44 +0530

    Create 36.cpp

---
+++ week2/36.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+     if (not height.size()) return 0;
+        int l = 0;
+        int r = height.size() - 1;
+        int left_max = 0, right_max = 0;
+        int ans = 0;
+            
+        while ( l < r ) {
+            if (height[l] < height[r]) {          
+                left_max = max(left_max, height[l]);
+                ans += left_max - height[l];
+                l++;
+            }
+            else {                                 
+                right_max = max(right_max, height[r]);
+                ans+= right_max - height[r];
+                r--;
+            }
+        }
+        return ans;
+        
+    }
+};
