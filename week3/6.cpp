9a920304e5f9b88794b924ed938c36e03dd969ca
SriyaNelluri <60146450+SriyaNelluri@users.noreply.github.com>
2021-04-14 10:09:44 +0530

    Create 6.cpp

---
+++ week3/6.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node {
+public:
+	int data;
+	Node* next;
+};
+
+void deleteList(Node** head_ref){
+	Node* current = *head_ref;
+	Node* next = NULL;
+	while (current != NULL){
+		next = current->next;
+		free(current);
+		current = next;
+	}
+	*head_ref = NULL;
+}
+void push(Node** head_ref, int new_data){
+	Node* new_node = new Node();
+	new_node->data = new_data;
+	new_node->next = (*head_ref);
+	(*head_ref) = new_node;
+}
+
+int main(){
+	Node* head = NULL;
+	push(&head, 1);
+	push(&head, 4);
+	push(&head, 1);
+	push(&head, 12);
+	push(&head, 1);
+	deleteList(&head);
+	cout << "Linked list deleted";
+}
+
