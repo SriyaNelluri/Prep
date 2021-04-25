3934c536c2388a690a8906d5d771a6cbb1ba3327
SriyaNelluri <60146450+SriyaNelluri@users.noreply.github.com>
2021-04-25 09:05:51 +0530

    Create merge.cpp

---
+++ week4/merge.cpp
@@ -0,0 +1,58 @@
+// { Driver Code Starts
+#include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+
+ // } Driver Code Ends
+class Solution
+{
+    public:
+    void merge(int Arr[], int start, int mid, int end){
+         // Your code here
+         int temp[end - start + 1];
+	int i = start, j = mid+1, k = 0;
+	while(i <= mid && j <= end) {
+		if(Arr[i] <= Arr[j]) {
+			temp[k] = Arr[i];
+			k += 1; i += 1;
+		}
+		else {
+			temp[k] = Arr[j];
+			k += 1; j += 1;
+		}
+	}
+	while(i <= mid) {
+		temp[k] = Arr[i];
+		k += 1; i += 1;
+	}
+	while(j <= end) {
+		temp[k] = Arr[j];
+		k += 1; j += 1;
+	}
+	for(i = start; i <= end; i += 1) {
+		Arr[i] = temp[i - start];
+	}
+    }
+    public:
+    void mergeSort(int Arr[], int start, int end){
+        //code here
+        if(start < end) {
+		int mid = (start + end) / 2;
+		mergeSort(Arr, start, mid);
+		mergeSort(Arr, mid+1, end);
+		merge(Arr, start, mid, end);
+    }
+    }
+};
