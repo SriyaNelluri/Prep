34413ec4b196928758a97023844bab731e7602d2
SriyaNelluri <60146450+SriyaNelluri@users.noreply.github.com>
2021-04-26 10:07:45 +0530

    Update 2c.cpp

--- week5/2c.cpp
+++ week5/2c.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int guessNumber(int n) {
-     int l= 1;
+   int l= 1;
         int r = n;
         while (l<=r) {
-            int mid = (l+r) / 2;
+            int mid = l+(r-l)/2;
             int ans = guess(mid);
             if (ans == 0)
                 return mid;
             else if (ans < 0)
-                high = mid - 1;
+                r = mid - 1;
             else
-                low = mid + 1;
+                l=mid + 1;
         }
         return -1;
         
