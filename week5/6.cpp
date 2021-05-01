f6a4835bf74e199a8abe4b17bd141e7ad773914c
SriyaNelluri <60146450+SriyaNelluri@users.noreply.github.com>
2021-05-01 20:45:10 +0530

    Create 6.cpp

---
+++ week5/6.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+  int n = letters.size();
+    
+    if(target >= letters[n - 1])
+        return letters[0];
+    int s = 0;
+    int e = n - 1;
+    
+    while(s < e)
+    {
+        int mid = s + (e - s) / 2;
+        if(letters[mid] > target)
+            e = mid;
+        else
+            s = mid + 1;
+    }
+    
+    return letters[s];
+    }
+};
