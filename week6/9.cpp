4bb56b89c944a3de1fb248671f05679d692a0d94
SriyaNelluri <60146450+SriyaNelluri@users.noreply.github.com>
2021-05-30 19:11:47 +0530

    Create 9.cpp

---
+++ week6/9.cpp
@@ -0,0 +1,65 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child */
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+    
+    return temp;
+}
+
+ // } Driver Code Ends
+/* A binary tree Node
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+ */
+
+
+class Solution
+{
+    public:
+    //Function to return the level order traversal of a tree.
+    vector<int> levelOrder(Node* root)
+    {
+      //Your code here
+      vector<int> res;
+       if (root == NULL)  return {0};
+    queue<Node *> q;
+
+    q.push(root);
+ 
+    while (q.empty() == false){
+        Node *node = q.front();
+        res.push_back(node->data);
+        q.pop();
+        if (node->left != NULL)
+            q.push(node->left);
+        if (node->right != NULL)
+            q.push(node->right);
+    }
+    return res;
+    }
+};
