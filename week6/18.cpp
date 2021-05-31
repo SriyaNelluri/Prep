29b1b999d12d43a4ad48db52690161ecc9249417
SriyaNelluri <60146450+SriyaNelluri@users.noreply.github.com>
2021-05-31 10:53:56 +0530

    Create 18.cpp

---
+++ week6/18.cpp
@@ -0,0 +1,51 @@
+public:
+void leafnodes(Node *root,vector<int>&v){
+    if(root==NULL)
+    return ;
+    leafnodes(root->left,v);
+    if(root->left==NULL && root->right==NULL)
+        v.push_back(root->data);
+    leafnodes(root->right,v);
+}
+
+void lb(Node*root,vector<int>&v){
+    if(root==NULL)
+    return ;
+    
+    if(root->left!=NULL || root->right!=NULL) 
+        v.push_back(root->data);
+    if(root->left!=NULL) 
+        lb(root->left,v);
+    else if(root->right!=NULL) 
+        lb(root->right,v);
+
+}
+
+void rb(Node*root,vector<int>&v){
+    if(root==NULL)
+    return ;
+    
+    if(root->right) 
+        rb(root->right,v);
+    else if(root->left) 
+        rb(root->left,v);
+    
+    if(root->left!=NULL || root->right!=NULL) 
+    v.push_back(root->data);
+
+}
+    vector <int> printBoundary(Node *root)
+    {
+        //Your code here
+        vector<int>v;
+if(root==NULL)
+return v;
+v.push_back(root->data);
+if(root->left==NULL && root->right==NULL)
+return v;
+lb(root->left,v);
+leafnodes(root,v);
+rb(root->right,v);
+        
+    }
+};
