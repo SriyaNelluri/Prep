3d2da8c74b72cddb365ed54ccbf878db7d447601
SriyaNelluri <60146450+SriyaNelluri@users.noreply.github.com>
2021-06-01 10:23:18 +0530

    Create 34.cpp

---
+++ week6/34.cpp
@@ -0,0 +1,33 @@
+
+    bool isCompleteBT(Node* root){
+        //code here
+        if (root == NULL)
+            return true;
+        queue<Node *> q;
+        q.push(root);
+      
+        bool flag = false;
+        while(!q.empty()){
+            Node *temp =q.front();
+            q.pop();
+     
+            if(temp->left){
+             if (flag == true)
+                return false;
+            q.push(temp->left);
+            }
+            else 
+              flag = true;
+     
+    
+            if(temp->right){
+                 if(flag == true)
+                    return false;
+            q.push(temp->right);
+            }
+            else 
+                flag = true;
+        }
+     
+        return true;
+    }
